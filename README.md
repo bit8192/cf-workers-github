8e6b353cfc3e5f1dd5551c97651390a8b8536f11
bincker <1229732681@qq.com>
2025-05-18 22:38:28 +0800

    实现能够访问releases版本信息和下载

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# Cloudflare Workers OpenAPI 3.1
+# cf-workers-github
 
-This is a Cloudflare Worker with OpenAPI 3.1 using [chanfana](https://github.com/cloudflare/chanfana) and [Hono](https://github.com/honojs/hono).
-
-This is an example project made to be used as a quick start into building OpenAPI compliant Workers that generates the
-`openapi.json` schema automatically from code and validates the incoming request to the defined parameters or request body.
+forward github releases、assets、raw
 
 ## Get started
 
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,57 @@
-import { fromHono } from "chanfana";
-import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
+function nginx() {
+	return new Response(`
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<title>Welcome to nginx!</title>
+	<style>
+		body {
+			width: 35em;
+			margin: 0 auto;
+			font-family: Tahoma, Verdana, Arial, sans-serif;
+		}
+	</style>
+	</head>
+	<body>
+	<h1>Welcome to nginx!</h1>
+	<p>If you see this page, the nginx web server is successfully installed and
+	working. Further configuration is required.</p>
+	
+	<p>For online documentation and support please refer to
+	<a href="http://nginx.org/">nginx.org</a>.<br/>
+	Commercial support is available at
+	<a href="http://nginx.com/">nginx.com</a>.</p>
+	
+	<p><em>Thank you for using nginx.</em></p>
+	</body>
+	</html>
+	`,
+		{
+			headers: {
+				'Content-Type': 'text/html; charset=UTF-8',
+			}
+		}
+	);
+}
 
-// Start a Hono app
-const app = new Hono<{ Bindings: Env }>();
+const REGEX_RELEASES = /^\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_.-]+\/releases(?:\/[a-zA-Z0-9_.-]+)+$/;
+const REGEX_ASSETS = /^\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_.-]+\/releases\/download(?:\/[a-zA-Z0-9_.-]+)+$/;
+const REGEX_RAW = /^\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_.-]+\/raw(?:\/[a-zA-Z0-9_.-]+)+$/;
 
-// Setup OpenAPI registry
-const openapi = fromHono(app, {
-	docs_url: "/",
-});
+export default {
+	fetch(request: Request<unknown, IncomingRequestCfProperties<unknown>>, env: Env, ctx: ExecutionContext): Response | Promise<Response> {
+		const url = new URL(request.url);
+		const matchType = [REGEX_RELEASES, REGEX_ASSETS, REGEX_RAW].findIndex(r=>url.pathname.match(r));
+		if (matchType < 0) return Promise.resolve(nginx());
+		const nextRequest = new Request(request.url.replace(url.origin, "https://github.com"), request);
+		if (matchType === 0) {
+			return fetch(nextRequest)
+				.then(response => new Request(response.headers.get('Location'), {
+					headers: {'Accept': 'application/json'},
+				}))
+				.then(req => fetch(req));
+		}
+		return fetch(nextRequest, {redirect: 'follow'});
+	}
 
-// Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
-
-// You may also register routes for non OpenAPI directly on Hono
-// app.get('/test', (c) => c.text('Hono!'))
-
-// Export the Hono app
-export default app;
+} as ExportedHandler<Env>
